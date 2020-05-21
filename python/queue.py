6269efc7cf4344cf1c4a0ff2f328b945283e9489
Michael Whalon <Michael.Whalon.CS@gmail.com>
2020-05-21 16:21:14 -0400

    Jason and Michael Data Structures

--- python/queue.py
+++ python/queue.py
@@ -1,14 +1,31 @@
 class Queue:
-  # write your __init__ method here that should store a 'total' value which is the total number of elements in the Queue and a 'queue' value which is an array of stored values in the Queue
+  def __init__(self):
+    self.length = 0
+    self.queue = []
 
-  def enqueue(self):
-    # write your code to add data to the Queue following FIFO and return the Queue
-    pass
 
-  def dequeue(self, data):
-    # write your code to removes the data to the Queue following FIFO and return the Queue
-    pass
+  def enqueue(self,data):
+    self.queue.append(data)
+    self.length += 1
+
+  def dequeue(self):
+    self.queue.pop(0)
+    self.length -= 1
 
   def size(self):
-    # write your code that returns the size of the Queue
-    pass
+    return self.length
+
+q =  Queue()
+a = 'a'
+b = 'b'
+c = 'c'
+d = 'd'
+
+print(q.queue)
+q.enqueue(a)
+q.enqueue(b)
+q.enqueue(c)
+q.enqueue(d)
+print(q.queue, q.length)
+q.dequeue()
+print(q.queue, q.length)
