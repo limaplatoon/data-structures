3d5968b0aab431d7de6bc76fe92412d37e71cde3
danielwhaleman <[whaleman@gmail.com]>
2020-05-21 20:44:45 -0500

    Dan Whaley and Eric Murphy Solution

--- python/queue.py
+++ python/queue.py
@@ -1,14 +1,17 @@
 class Queue:
-  # write your __init__ method here that should store a 'total' value which is the total number of elements in the Queue and a 'queue' value which is an array of stored values in the Queue
+  def __init__(self):
+    self.queue = []
+    self.total = len(self.queue)
+  
+  def enqueue(self, value):
+    self.queue.insert(0, value)
+    self.total = len(self.queue)
 
-  def enqueue(self):
-    # write your code to add data to the Queue following FIFO and return the Queue
-    pass
-
-  def dequeue(self, data):
-    # write your code to removes the data to the Queue following FIFO and return the Queue
-    pass
+  def dequeue(self):
+    self.queue.pop(-1)
 
   def size(self):
-    # write your code that returns the size of the Queue
-    pass
+    return self.total
+
+
+
--- python/stack.py
+++ python/stack.py
@@ -1,14 +1,17 @@
 class Stack:
-  # write your __init__ method here that should store a 'total' value which is the total number of elements in the Stack and a 'stack' value which is an array of stored values in the Stack
+  def __init__(self):
+    self.stack = []
+    self.total = len(self.stack)
+    
+  def push(self, data):
+    self.stack.append(data)
 
-  def push(self):
-    # write your code to add data following LIFO and return the Stack
-    pass
-
-  def pop(self, data):
-    # write your code to removes the data following LIFO and return the Stack
-    pass
+  def pop(self):
+    self.stack.pop()
 
   def size(self):
-    # write your code that returns the size of the Stack
-    pass
+    return self.total
+    
+    
+
+
