ce010915f0a9edb2fa5386c921abcf24f8d14217
Julian West <west221b@gmail.com>
2020-05-22 22:24:05 -0400

    Add Stack, Queue, LinkedList w/print

--- python/queue.py
+++ python/queue.py
@@ -1,14 +1,19 @@
 class Queue:
-  # write your __init__ method here that should store a 'total' value which is the total number of elements in the Queue and a 'queue' value which is an array of stored values in the Queue
+    # write your __init__ method here that should store a 'total' value which is the total number of elements in the Queue and a 'queue' value which is an array of stored values in the Queue
 
-  def enqueue(self):
-    # write your code to add data to the Queue following FIFO and return the Queue
-    pass
+    def __init__(self):
+        self.total = 0  # ??
+        self.queue = []
 
-  def dequeue(self, data):
-    # write your code to removes the data to the Queue following FIFO and return the Queue
-    pass
+    def enqueue(self, data):
+        # write your code to add data to the Queue following FIFO and return the Queue
+        self.queue.append(data)
+        return self.queue
 
-  def size(self):
-    # write your code that returns the size of the Queue
-    pass
+    def dequeue(self):
+        # write your code to removes the data to the Queue following FIFO and return the Queue
+        return self.queue.pop(0)
+
+    def size(self):
+        # write your code that returns the size of the Queue
+        return len(self.queue)
